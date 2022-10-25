6790d6720d016e33179c20d14a66c7b75f511df4
Denner <denner@DESKTOP-BGSGB4A.localdomain>
2022-10-25 19:16:00 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.tsx", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Roboto, sans-serif",
+      },
+      colors: {
+        white: {
+          100: "#ffffff",
+          500: "#F8F8F8",
+        },
+        gray: {
+          500: "#292929",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
