1e58b1b81a7e7d8faf6eb05d05c7e0102765a799
Denner Nascimento <jose.denner.nascimento@gmail.com>
2022-10-25 23:58:10 -0300

    feat: adding config files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.tsx", "./index.html"],
+  content: ['./src/**/*.tsx', './index.html'],
   theme: {
     extend: {
       fontFamily: {
-        sans: "Roboto, sans-serif",
+        sans: 'Roboto, sans-serif',
       },
       colors: {
         white: {
-          100: "#ffffff",
-          500: "#F8F8F8",
+          100: '#ffffff',
+          500: '#F8F8F8',
         },
         gray: {
-          500: "#292929",
+          500: '#292929',
         },
       },
     },
